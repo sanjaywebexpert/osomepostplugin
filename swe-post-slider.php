b06e24147e61de712c7dcc5c928c8caa71a869ae
Sanjay second Technical <Sanjaysecond@gmail.com>
2019-07-05 13:57:04 +0530

    shortcode and setting function added

--- swe-post-slider.php
+++ swe-post-slider.php
@@ -34,6 +34,15 @@ function swe_osm_add_slider_menu(){
 	);
 }
 add_action( 'admin_init', 'swe_osm_options_init' );
+function swe_osm_options_init(){
+	register_setting( 'swe_osm_options', 'swe_osm_slider_options', 'swe_osm_options_validate' );
+}
+add_action("admin_menu","swe_osm_add_slider_menu");
+function swe_osm_post_slider_func(){	
+	include_once SWE_OPS_PLUGIN_DIR_PATH.'/inc/osomepost_how_to_use.php';
+}
+function swe_osm_setting_function(){
+	include_once SWE_OPS_PLUGIN_DIR_PATH.'/inc/osomepost_setting.php';
 }
 /*  Registered All the Scripts and Styles */
 function swe_osm_post_slider_script(){
@@ -43,3 +52,19 @@ function swe_osm_post_slider_script(){
 	wp_register_script("swe_osm_post_slider_js",plugins_url('/assets/js/swe.owl.carousel.js',__FILE__), array(), '1.0.0', true);
 }
 add_action('init','swe_osm_post_slider_script');
+
+
+function swe_osm_load_admin_style_forslider() {
+        wp_register_style( 'custom_slider_admin_css', plugins_url('/assets/css/admin-style.css',__FILE__) );
+        wp_enqueue_style( 'custom_slider_admin_css' );
+}
+add_action( 'admin_enqueue_scripts', 'swe_osm_load_admin_style_forslider' );
+
+// Post thumbnails
+if (function_exists('add_theme_support')) {
+  add_theme_support('post-thumbnails');
+}
+add_image_size( 'post-slider-thumb', 600, 400, true );
+
+/* Include Shortcode Function File  */
+include_once SWE_OPS_PLUGIN_DIR_PATH.'/inc/osomepost_shortcode.php';
