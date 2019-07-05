0dc25100388c02c62042cdb83a9dc6f961e106b5
Sanjay second Technical <Sanjaysecond@gmail.com>
2019-07-05 14:10:20 +0530

    How to use file changed

--- inc/osomepost_how_to_use.php
+++ inc/osomepost_how_to_use.php
@@ -11,10 +11,5 @@
 	<h3  style="color:black;">To use This Plugin , Use Shortcode to Display Slider on any post , page and widget </h3>
 	<h3 class="shortcode">[osomepost_slider]</h3>
 	<span class="or">or</span>
-	<h2>Post Slider with Category</h2>
-	<h3 class="shortcode">[osomepost_slider category="id"] </h3>
-	<h3 class="shortcode">like [osomepost_slider category="2"] </h3>
-	<span class="or">or</span>
-	<h3  style="color:black;">Use this php code to use slider in php file </h3>
-	<h3 class="shortcode"><pre>&lt;?php echo do_shortcode('[osomepost_slider]'); ?&gt;</pre></h3>
+	
 </div>
